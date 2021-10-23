1fdfa2eb743996eaa6abc73789d07fd51d1b30f8
Jeremy <61841422+Jerry3T@users.noreply.github.com>
2021-10-23 17:05:32 -0400

    immense suffering

---
+++ D_4kyu_rangeExtraction.cpp
@@ -0,0 +1,81 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+std::string range_extraction(std::vector<int> args) {
+  
+  vector<int> copy = args;
+  copy.push_back(32405342);
+  int length = copy.size();
+  string endResult = "";
+  int termination = 0;
+  int lengthCount = 1;
+  
+  while (length > 0) {
+    
+    int start = 0;
+    lengthCount = 1;
+
+    for (int z = 0; z < length - 1; z++) {
+      
+      cout << z;
+      cout << ", ";
+      
+      if (copy.at(z)+1==copy.at(z+1)) {
+
+        lengthCount+=1;
+        termination = z+1;
+
+      } else {
+        
+        if (z+1==32405342) {
+          
+          termination = z;
+          
+        }
+        termination = z+1;
+
+        break;
+      }
+    }
+
+    if (lengthCount >= 3) { 
+      
+      endResult+=to_string(copy.at(start)) + "-" + to_string(copy.at(termination-1)) + ",";
+
+      
+    } else if (lengthCount == 2) {
+      
+      endResult+=to_string(copy.at(start)) + "," + to_string(copy.at(termination-1)) + ",";
+      
+    } else {
+      
+      endResult+=to_string(copy.at(start)) + ",";    
+    
+    }
+
+    if (length - termination == 1) {
+      
+      copy.erase(copy.begin() + 0, copy.begin() + termination+1);
+      
+    } else copy.erase(copy.begin() + 0, copy.begin() + termination);
+    
+    length = copy.size();
+    termination = 0;
+    
+    for (int s : copy) {
+
+      cout << s;
+      cout << ", ";
+
+    }
+    
+    cout << "\n";
+
+  }
+  
+  endResult.pop_back();
+  return endResult;
+  
+}
